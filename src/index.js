44b52df72645e9860419045a2a84cfd53b09fdca
Alkhimenok Kirill <alkhimenok.kirill.leonidovich@gmail.com>
2021-10-14 12:58:49 +0300

    add: gitignore file

--- src/index.js
+++ src/index.js
@@ -6,29 +6,31 @@ const markup = `
   <div class="wrapper">
     <div class="select">
       <div class="select__container">
-        <section class="select__header">
-          <section class="select__heading">
-            <h2 class="select__title">
-              Тендеры в роли Поставщика
-            </h2>
-            <a class="select__btn_show-selected" href="#">
-              Показать выбранное (10)
-            </a>
+        <div class="select__body">
+          <section class="select__header">
+            <section class="select__heading">
+              <h2 class="select__title">
+                Тендеры в роли Поставщика
+              </h2>
+              <a class="select__btn_show-selected" href="#">
+                Показать выбранное (10)
+              </a>
+            </section>
+            <section class="select__selected-options">
+              <input class="select__input" type="text" disabled value="Код ОКРБ или наименование закупаемой продукции">
+            </section>
           </section>
-          <section class="select__selected-options">
-            <input class="select__input" type="text" disabled value="Код ОКРБ или наименование закупаемой продукции">
+          <section class="select__main">
           </section>
-        </section>
-        <section class="select__main">
-        </section>
-        <section class="select__footer">
-          <button class="select__btn_to-apply">
-            применить
-          </button>
-          <button class="select__btn_clear">
-            Очистить
-          </button>
-        </section>
+          <section class="select__footer">
+            <button class="select__btn_to-apply">
+              применить
+            </button>
+            <button class="select__btn_clear">
+              Очистить
+            </button>
+          </section>
+        </div>
       </div>
     </div>
   </div>
